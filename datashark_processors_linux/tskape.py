cd0bb12415d3532a5bd62aed42e076a31233c5de
koromodako <koromodako@gmail.com>
2021-09-05 01:07:35 +0200

    Update

--- datashark_processors_linux/tskape.py
+++ datashark_processors_linux/tskape.py
@@ -3,8 +3,8 @@
 from typing import List, Tuple, Optional
 from pathlib import Path
 from datashark_core.meta import ProcessorMeta
-from datashark_core.plugin import ProcessorInterface
 from datashark_core.logging import LOGGING_MANAGER
+from datashark_core.processor import ProcessorInterface
 from datashark_core.model.api import System, ProcessorArgument
 
 NAME = 'linux_tskape'
@@ -26,6 +26,7 @@ class TSKAPEProcessor(ProcessorInterface, metaclass=ProcessorMeta):
     ) -> Tuple[bool, Optional[str]]:
         """Process a file using tskape"""
         status = False
+        details = None
         try:
             # TODO: perform artifact processing here
             raise NotImplementedError()
