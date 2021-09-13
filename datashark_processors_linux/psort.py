4312d54f09ed4ce4b548638d88166d1dcfe6dfde
koromodako <koromodako@gmail.com>
2021-09-13 21:46:22 +0200

    Improve generic processor

--- datashark_processors_linux/psort.py
+++ datashark_processors_linux/psort.py
@@ -111,6 +111,4 @@ class PSortProcessor(ProcessorInterface, metaclass=ProcessorMeta):
             stdout=DEVNULL,
             stderr=PIPE,
         )
-        _, stderr = await proc.communicate()
-        if proc.returncode != 0:
-            raise ProcessorError(stderr)
+        await self._handle_communicating_process(proc)
